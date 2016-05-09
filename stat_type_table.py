e026190b93604f9082ce637d32dfc9ca6f0a0d0a
Gus Smith <hfs5022@psu.edu>
2016-05-09 09:56:40 -0400

    adds stat_type_table

---
+++ stat_type_table.py
@@ -0,0 +1,25 @@
+class StatTypeTable:
+	schema = {
+		'name' : 'stat_type',
+		'columns' : [
+			{
+				'column_name' :  		'name',
+				'datatype' : 				'VARCHAR(255)'
+			},
+			{
+				'column_name' : 		'game_id',
+				'datatype' : 				'INTEGER'
+			},
+			{
+				'column_name' :	 		'id',
+				'datatype' : 				'INTEGER'
+			}
+		],
+		'constraints' : [
+			{
+				'name':							'pk_id',
+				'type':							'PRIMARY KEY',
+				'columns':					['id']
+			}
+		]
+	}
