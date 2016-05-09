381ecec0c99435c3428e0fbaaa369e2c5f1f3452
Gus Smith <hfs5022@psu.edu>
2016-05-09 11:59:15 -0400

    Defines stat table schema

--- stat_table.py
+++ stat_table.py
@@ -1,1 +1,36 @@
 class StatTable:
+	schema = {
+		'name' : 'stat_type',
+		'columns' : [
+			{
+				'column_name'			:			'value',
+				'datatype'				:			'INTEGER'
+			},		
+			{		
+				'column_name' 		: 		'character_id',
+				'datatype' 				: 		'INTEGER'
+			},		
+			{		
+				'column_name'			:			'game_id',
+				'datatype'				:			'INTEGER'
+			},		
+			{		
+				'column_name' 		:	 		'id',
+				'datatype' 				: 		'INTEGER'
+			}		
+		],		
+		'constraints' : [		
+			{		
+				'name'						:			'pk_id',
+				'type'						:			'PRIMARY KEY',
+				'columns'					:			['id']
+			},		
+			{		
+				'name'						:			'fk_character',
+				'type'						:			'FOREIGN KEY',
+				'columns'					:			['character_id'],
+				'foreign-table'		:			'character',
+				'foreign-columns'	:			['id']
+			}
+		]
+	}
