cdd527110931cbc9ae764d98691213b5b44c0ee5
Gus Smith <hfs5022@psu.edu>
2016-06-12 18:55:08 -0700

    Adds add_owner_to_game

--- game_owner_table.py
+++ game_owner_table.py
@@ -40,3 +40,13 @@ class GameOwnerTable:
 	
 	def __init__(self, db):
 		self._db = db
+		
+	# This does not handle creating the owner/game beforehand.
+	def add_owner_to_game(self, game_id, owner_id):
+		query = "INSERT INTO " + self.schema['name'] \
+			+ " (" + OWNER_ID_COLUMN_KEY + "," + GAME_ID_COLUMN_KEY + ") "\
+			+ "VALUES (?,?)"
+		return_list = []
+		self._db.add_query( (query, [owner_id,game_id], return_list) )
+		while len(return_list) == 0: pass
+		return return_list
