b328ba1bf8220eb8a9ca60524e380fe7374b314b
Gus Smith <hfs5022@psu.edu>
2016-07-21 22:20:43 -0700

    adds some documentation

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 class Utils:
 	def db_results_to_dict(columns, results):
+		""" Return a dictionary with the column names as keys, and values from the
+		results list.
+		
+		There are two preconditions which must be true about the arguments for the
+		output to be correct:
+		1. There must be exactly one result for each column. If there are less 
+				results than columns, an exception will be thrown. If there are more,
+				some of the data in results will not be in the returned dictionary.
+		2. The results and the list of columns must be in the same order. 
+		"""
 		return_dict = {}
 		for counter, column in enumerate(columns):
 			val = results[counter]
